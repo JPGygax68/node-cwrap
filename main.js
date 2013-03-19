b90a7c02972e00ae9d754b6ef60fd9a6c1348366
Hans-Peter Gygax <gygax@practicomp.ch>
2013-03-19 22:17:41 +0100

    RegExp for #define extraction

--- main.js
+++ main.js
@@ -12,10 +12,19 @@ fs.read('./test/lsdisplay2.h')
   }
   else warn('This header file does not appear to have a guard against multiple inclusion');
   
-  // Function declarations
-  var re = /^(?!#)((?:[ \t\n]*\b\w+)(?:[ \t\n]+\b\w+)+)[ \t\n]*(\b\w+)[ \t\n]*\(([^\)]*)\)[ \t\n]*;/gm, m;
+  // Constant #defines
+  var re = /^[ \t]*#[ \t]*define *(\b\w+)[ \t]+((.(?!\/\/))*)$/gm, m;
+  log('Constant defines:');
   while ((m = re.exec(content)) !== null) {
-    log('Function signature found:', m[1], ' | ', m[2], ' | ', m[3]);
+    log(m[1], m[2]);
+  }
+  
+  // Function declarations (m[1] = before name, m[2] = name, m[3] = parameters (without parens) )
+  var re = /^(?!#)((?:[ \t\n]*\b\w+)(?:[ \t\n]+\b\w+)+)[ \t\n]*(\b\w+)[ \t\n]*\(([^\)]*)\)[ \t\n]*;/gm, m;
+  log('Function signatures:');
+  while ((m = re.exec(content)) !== null) {
+    log(m[1], m[2], '(', m[3], ')');
+    // TODO: filter the return type
   }
 });
 
