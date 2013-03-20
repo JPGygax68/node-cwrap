1d7fb5578942bc49d8ee3494355d6372826c8d46
Hans-Peter Gygax <gygax@practicomp.ch>
2013-03-20 12:20:19 +0100

    Filtering constants

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 var cintfdesc = require("./cintfdesc");
 var fs        = require("q-io/fs");
 
+var constants = {};
+var functions = {};
+
 fs.read('./test/lsdisplay2.h')
 .then( function(content) {
   // Multiple inclusion guard
@@ -14,17 +17,34 @@ fs.read('./test/lsdisplay2.h')
   
   // Constant #defines
   var re = /^[ \t]*#[ \t]*define *(\b\w+)[ \t]+((.(?!\/\/))*)$/gm, m;
-  log('Constant defines:');
+  log('\nConstant defines:');
+  log(  '----------------');
   while ((m = re.exec(content)) !== null) {
-    log(m[1], m[2]);
+    // Filter out definitions that are not constants
+    var name  = m[1];
+    var value = checkConstant(m[2]);
+    if (value !== null) { constants[name] = value; console.log(name, '=', value); }
   }
   
   // Function declarations (m[1] = before name, m[2] = name, m[3] = parameters (without parens) )
   var re = /^(?!#)((?:[ \t\n]*\b\w+)(?:[ \t\n]+\b\w+)+)[ \t\n]*(\b\w+)[ \t\n]*\(([^\)]*)\)[ \t\n]*;/gm, m;
-  log('Function signatures:');
+  log('\nFunction signatures:');
+  log(  '-------------------');
   while ((m = re.exec(content)) !== null) {
     log(m[1], m[2], '(', m[3], ')');
     // TODO: filter the return type
+  }
+  
+  //----
+  
+  function checkConstant(expr) {
+    var expr = expr.trim();
+    var re = /^L?\"[^"]*$/;
+    var m = re.exec(expr);
+    if (m) return '"' + m[1] + '"';
+    var value = parseFloat(expr);
+    if (value.toString() !== 'NaN') return value;
+    return null;
   }
 });
 
