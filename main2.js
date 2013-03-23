0fe8cdf8e98205b399d4ce09347d01c1a4a03d5c
Hans-Peter Gygax <gygax@practicomp.ch>
2013-03-23 14:07:14 +0100

    Implemented constant extraction

--- main2.js
+++ main2.js
@@ -4,28 +4,56 @@ var fs     = require("q-io/fs");
 var Dom    = require("xmldom").DOMParser;
 var xpath  = require("xpath");
 
-fs.read('./test/lsdisplay2_wrap.xml')
+var input_path = process.argv[2];
+//console.log(input_path);
+
+// Interface description
+var intf = { functions: {}, constants: {} };
+  
+// TODO: integrate SWIG ?
+fs.read(input_path)
 .then( function(content) {
+
   // Repair the closing tags (which have a space at the end, tsk tsk)
   content = content.replace(/<\/(\w+)\s+>/gm, '</$1>');
   // Parse the whole thing
   var doc = new Dom().parseFromString(content);
-  // Now let's try a query
+  
+  // Get the functions
   var attrib_lists = xpath.select('//cdecl/attributelist/attribute[@name="kind"][@value="function"]/..', doc);
   attrib_lists.forEach( function(attrib_list) {
-    var name_attr   = xpath.select('./attribute[@name="name"]/@value', attrib_list)[0];
-    var type_attr   = xpath.select('./attribute[@name="type"]/@value', attrib_list)[0];
+    // Extract the function name
+    var name = xpath.select('./attribute[@name="name"]/@value', attrib_list)[0].value;
+    // Create and add a new function descriptor
+    var func = { params: {} };
+    intf.functions[name] = func;
+    // Extract and store the data
+    func.type = xpath.select('./attribute[@name="type"]/@value', attrib_list)[0].value;
     var param_nodes = xpath.select('./parmlist/parm', attrib_list);
-    log(name_attr.value, ':', type_attr.value);
-    param_nodes.forEach( function(param_node) {
+    param_nodes.forEach( function(param_node, i) {
       var name_attr = xpath.select('./attributelist/attribute[@name="name"]/@value', param_node)[0];
-      // If no "name" attribute was found, then the function has no parameters
-      if (name_attr) {
-        var type_attr = xpath.select('./attributelist/attribute[@name="type"]/@value', param_node)[0];      
-        console.log('  ', name_attr.value, ':', type_attr.value);
-      }
+      var type_attr = xpath.select('./attributelist/attribute[@name="type"]/@value', param_node)[0];      
+      var key = name_attr ? name_attr.value : i.toString();
+      func.params[key] = { type: type_attr.value };
     });
   });
+  
+  // Get the constants
+  var attrib_lists = xpath.select('//constant/attributelist/attribute[@name="storage"][@value="%constant"]/..', doc);
+  attrib_lists.forEach( function(attrib_list) {
+    // Extract the function name
+    var name = xpath.select('./attribute[@name="name"]/@value', attrib_list)[0].value;
+    // Create and add a new function descriptor
+    var constant = { };
+    intf.constants[name] = constant;
+    // Extract and store the data
+    constant.type  = xpath.select('./attribute[@name="type"]/@value' , attrib_list)[0].value;
+    constant.value = xpath.select('./attribute[@name="value"]/@value', attrib_list)[0].value;
+  });
+  
+  // Done.
+  //console.log( JSON.stringify(intf, null, '\t') );
+  console.log( JSON.stringify(intf.constants, null, '\t') );
 });
 
 //----------
