b00f21db5c46b3b43b2ecd4756f4a2a4138e3159
Hans-Peter Gygax <gygax@practicomp.ch>
2013-05-27 22:55:47 +0200

    Tests were coded without assert's - duh...

--- tests/test.js
+++ tests/test.js
@@ -8,11 +8,16 @@ describe('parsing', function() {
   before( function(done) {
     this.timeout(10000);
     cwrap.parse('./tests/squish-1.11/squish.h')
-      .then( function(intf_) { intf = intf_; console.log(intf); done(); } );
+      .then( function(intf_) { intf = intf_; done(); } );
   });
   
   describe('interface object', function() {
-    it('should have a functions property', function() { return !!intf.functions; } );
+    it('should have a "functions" property', function() { assert(!!intf.functions); } );
+    it('should have a "constants" property', function() { assert(!!intf.constants); } );
+    it('should have a "classes" property', function() { assert(!!intf.classes); } );
+    it('should have found a function "squish::Compress"', function() { assert(!!intf.functions['squish::Compress']); } );
+    it('should have found a function "squish::Decompress"', function() { assert(!!intf.functions['squish::Compress']); } );
+    it('should have found a constant "squish::kDxt1"', function() { assert(!!intf.constants['squish::kDxt1']); } );
   });
   
 });
